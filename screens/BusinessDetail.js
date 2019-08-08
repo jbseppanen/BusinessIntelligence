a6c54eaf5d30cf1c8f590a53262b38539fc4d27f
Jacob Seppanen <jbseppanen@gmail.com>
2019-08-08 16:54:39 -0700

    Display more of the data.

--- screens/BusinessDetail.js
+++ screens/BusinessDetail.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 
 export default class BusinessDetail extends React.Component {
   render() {
     const { navigation } = this.props;
     const itemId = navigation.getParam('id', 'NO-ID');
     const name = navigation.getParam('name', 'unknown');
+    const location = navigation.getParam('location', 'unknown');
+    const revenue = navigation.getParam('revenue', []);
 
-    return <Text>{name}</Text>
-    // return <View />;
+
+    return <View>
+      <Text>{name}</Text>
+      <Text>{location.address}</Text>
+      <Text>{location.city}</Text>
+      <Text>{location.country}</Text>
+      <FlatList
+        data={revenue}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) =>
+          <View>
+            <Text>Date: {item.date}</Text>
+            <Text>Value: {item.value}</Text>
+          </View>
+        }
+        keyExtractor={item => item.seq.toString()}
+      />
+
+
+    </View>
   }
 }
