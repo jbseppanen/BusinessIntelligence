b6d37406f0e883800c9f06183812fb95058d967a
Jacob Seppanen <jbseppanen@gmail.com>
2019-08-08 15:04:47 -0700

    Build project.  Read data.json file in Business.js

--- screens/Businesses.js
+++ screens/Businesses.js
@@ -3,7 +3,9 @@ import { FlatList, Text } from 'react-native';
 
 export default class Businesses extends React.Component {
   render() {
-    return <Text>Foo bar shibby</Text>
+    const data = require("../data");
+    let companyList = data.map(a => a.name)
+    return <Text>{companyList[0]}</Text>
     // return <FlatList />
   }
 }
