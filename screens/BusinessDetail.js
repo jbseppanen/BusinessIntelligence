f042098e830be9087618555c77d0c9e0814fd3f2
Jacob Seppanen <jbseppanen@gmail.com>
2019-08-16 09:34:17 -0700

    Update BusinessDetail to show chart.

--- screens/BusinessDetail.js
+++ screens/BusinessDetail.js
@@ -1,33 +1,43 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import RNChartView from './ChartView'
 
 export default class BusinessDetail extends React.Component {
   render() {
     const { navigation } = this.props;
-    const itemId = navigation.getParam('id', 'NO-ID');
     const name = navigation.getParam('name', 'unknown');
     const location = navigation.getParam('location', 'unknown');
     const revenue = navigation.getParam('revenue', []);
 
-
-    return <View>
-      <Text>{name}</Text>
-      <Text>{location.address}</Text>
+    return <View style={styles.container}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.address}>{location.address}</Text>
       <Text>{location.city}</Text>
       <Text>{location.country}</Text>
-      <FlatList
-        data={revenue}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) =>
-          <View>
-            <Text>Date: {item.date}</Text>
-            <Text>Value: {item.value}</Text>
-          </View>
-        }
-        keyExtractor={item => item.seq.toString()}
-      />
-
-
+      <View style={{ flex: 2 }}>
+        <RNChartView
+          style={{ flex: 2 }}
+          data={{ axisLabel: "Revenue", dataList: revenue, description: "Months" }}
+        />
+      </View>
     </View>
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 0,
+    justifyContent: 'center',
+    // backgroundColor: '#F5FCFF',
+  },
+
+  name: {
+    fontFamily: 'Verdana',
+    fontSize: 20
+  },
+  address: {
+    fontFamily: 'Helvitica',
+    fontSize: 16
+  }
+});
