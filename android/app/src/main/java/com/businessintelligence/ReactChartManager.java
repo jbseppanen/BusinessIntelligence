6d1493ad4e88c27955e4066ee1aeea950dd6e637
Jacob Seppanen <jbseppanen@gmail.com>
2019-08-16 10:53:19 -0700

    Reverse chart to make oldest data on the left. Update month labels on chart.

--- android/app/src/main/java/com/businessintelligence/ReactChartManager.java
+++ android/app/src/main/java/com/businessintelligence/ReactChartManager.java
@@ -1,5 +1,7 @@
 package com.businessintelligence;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.uimanager.SimpleViewManager;
@@ -15,6 +17,7 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import javax.annotation.Nonnull;
 
@@ -37,24 +40,29 @@ public class ReactChartManager extends SimpleViewManager<LineChart> {
 
     @ReactProp(name = "data")
     public void setData(LineChart chart, ReadableMap map) {
-        ArrayList<Entry> entries = new ArrayList<>();
         ArrayList<String> xLabels = new ArrayList<>();
         ReadableArray dataList = map.getArray("dataList");
         if (dataList != null) {
-            for (int i = 0; i < dataList.size(); i++) {
+            ArrayList<Entry> entries = new ArrayList<>(dataList.size());
+            int j = 0;
+            for (int i = (dataList.size() - 1); i >= 0; i--) {
+
                 ReadableMap item = dataList.getMap(i);
                 assert item != null;
-                entries.add(new Entry(item.getInt("seq"), (float) item.getDouble("value")));
+                entries.add(new Entry((j), (float) item.getDouble("value")));
 
-                String dateString = item.getString("date");
-                String month = dateString.split("-")[1];
-                xLabels.add(month);
+                //Extract date and split off time component.
+                String dateString = item.getString("date").split(" ")[0];
+                String[] dateArray = dateString.split("-");
+
+                xLabels.add(String.format(Locale.getDefault(), "%s-%s", dateArray[1], dateArray[0]));
+                j++;
             }
 
             ValueFormatter formatter = new ValueFormatter() {
                 @Override
                 public String getAxisLabel(float value, AxisBase axis) {
-                 String[] arr =  xLabels.toArray(new String[xLabels.size()]);
+                    String[] arr = xLabels.toArray(new String[xLabels.size()]);
                     return arr[(int) value];
                 }
             };
